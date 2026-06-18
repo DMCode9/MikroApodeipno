4eed79f54b7d29b867b68540fe7aa3d430d53005
DMCode9 <digitalmissioncode@gmail.com>
2026-06-18 09:06:32 +0300

    v11

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mikro-apodeipno-v10';
+const CACHE_NAME = 'mikro-apodeipno-v11';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
@@ -13,7 +13,18 @@ const ASSETS_TO_CACHE = [
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS_TO_CACHE);
+      // Χρησιμοποιούμε fetch με 'no-cache' για να παρακάμψουμε την HTTP cache
+      // και να πάρουμε σίγουρα τις τελευταίες εκδόσεις των αρχείων
+      return Promise.all(
+        ASSETS_TO_CACHE.map((url) => {
+          return fetch(new Request(url, { cache: 'no-cache' })).then((response) => {
+            if (!response.ok) {
+              throw new Error('Failed to fetch ' + url);
+            }
+            return cache.put(url, response);
+          });
+        })
+      );
     })
   );
 });
